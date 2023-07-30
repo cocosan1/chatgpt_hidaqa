df1cae6e7524cbb730e9b2f5021b7fbe02859f98
hskw1 <isishskw@gmail.com>
2023-07-30 20:35:56 +0900

    3rd commit

--- main.py
+++ main.py
@@ -203,17 +203,7 @@ def check_doc1():
     st.write(documents)
 
 def qa_calc():
-    
-    #質問の入力
-    question0 = st.text_input('キーフレーズを入力してください', key='question')
-    question = question0 + 'に関連する情報を教えてください。'
-
-    if not question0:
-        st.info('質問を入力してください')
-        st.stop()
-    
     clear_chat = st.button("履歴消去", key='clear_chat')
-
     # チャット履歴を保存
     if "chat_history" not in st.session_state:
         st.session_state["chat_history"] = []
@@ -221,117 +211,129 @@ def qa_calc():
     if clear_chat:
         st.session_state["chat_history"] = []
         st.stop()
+
+    with st.form('入力'):
+            #質問の入力
+        question0 = st.text_input('キーフレーズを入力してください', key='question')
+        question = question0 + 'に関連する情報を教えてください。'
+        num_node = st.number_input('抽出ノード数を指定してください', value=1, key='num_node')
+
+        submitted = st.form_submit_button('submitted')
     
-    #ストレージからindexデータの読み込み
-    @st.cache_data(ttl=datetime.timedelta(hours=1))
-    def read_storage():
-        storage_context = StorageContext.from_defaults(
-            docstore=SimpleDocumentStore.from_persist_dir(persist_dir="./storage_context1"),
-            vector_store=SimpleVectorStore.from_persist_dir(persist_dir="./storage_context1"),
-            index_store=SimpleIndexStore.from_persist_dir(persist_dir="./storage_context1"),
+    if submitted:
+
+        
+        
+        #ストレージからindexデータの読み込み
+        @st.cache_data(ttl=datetime.timedelta(hours=1))
+        def read_storage():
+            storage_context = StorageContext.from_defaults(
+                docstore=SimpleDocumentStore.from_persist_dir(persist_dir="./storage_context1"),
+                vector_store=SimpleVectorStore.from_persist_dir(persist_dir="./storage_context1"),
+                index_store=SimpleIndexStore.from_persist_dir(persist_dir="./storage_context1"),
+            )
+            return storage_context
+        
+        storage_context = read_storage()
+
+        # 実装時点でデフォルトはtext-ada-embedding-002
+        #文書テキストを埋め込みベクトルに変換するためのモデル（事前学習済みのニューラルネットワークモデル）
+        embed_model = OpenAIEmbedding()
+
+        #埋め込みモデルによるテキスト埋め込みを生成
+        #埋め込みベクトルを保持するためのリスト
+        docs = []
+        #文書のIDとテキストの対応を保持するための辞書
+        id_to_text_map = {}
+        #文書データを格納しているストレージコンテキストから文書の一覧を取得
+        for i, (_, node) in enumerate(storage_context.docstore.docs.items()):
+            #文書ノード（node）からテキストを取得
+            text = node.get_text()
+            #テキストの埋め込みを生成します
+            docs.append(embed_model.get_text_embedding(text))
+            id_to_text_map[i] = text
+        docs = np.array(docs)
+
+        #text-ada-embedding-002から出力されるベクトル長を指定
+        d = 1536
+        index = faiss.IndexFlatL2(d)
+        #Faissにベクトルを登録
+        index.add(docs)
+
+        # クエリとFaissから取り出すノード数の設定
+        query_text = question
+        k = num_node
+
+        # questionのベクトル化
+        query = embed_model.get_text_embedding(query_text)
+        query=np.array([query])
+
+        # Faissからのquestionに近いノードの取り出し2個
+        reader = FaissReader(index)
+        documents = reader.load_data(query=query, id_to_text_map=id_to_text_map, k=k)
+
+        st.write(f'count_node: {len(documents)}')
+
+        
+        # デバッグ用
+        llama_debug_handler = LlamaDebugHandler()
+        callback_manager = CallbackManager([llama_debug_handler])
+        service_context = ServiceContext.from_defaults(callback_manager=callback_manager)
+
+        #Faissで確認した類似したノードを使って、GPTListIndexを作成。
+        # index = GPTListIndex.from_documents(documents, service_context=service_context)
+        index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
+
+        #質問用のQAプロンプトを生成
+        QA_PROMPT_TMPL = (
+            "私たちは以下の情報をコンテキスト情報として与えます。 \n"
+            "---------------------\n"
+            "{context_str}"
+            "\n---------------------\n"
+            "あなたはAIとして、この情報をもとに質問を日本語で答えます。前回と同じ回答の場合は同じ回答を行います。: {query_str}\n"
         )
-        return storage_context
-    
-    storage_context = read_storage()
+        qa_prompt = QuestionAnswerPrompt(QA_PROMPT_TMPL)
 
-    # 実装時点でデフォルトはtext-ada-embedding-002
-    #文書テキストを埋め込みベクトルに変換するためのモデル（事前学習済みのニューラルネットワークモデル）
-    embed_model = OpenAIEmbedding()
+        ###############################################################tets
+        #回答要求用のプロンプトを生成
+        REFINE_PROMPT = ("元の質問は次のとおりです: {query_str} \n"
+            "既存の回答を提供しました: {existing_answer} \n"
+            "既存の答えを洗練する必要があります \n"
+            "(必要な場合のみ)以下にコンテキストを追加します。 \n"
+            "------------\n"
+            "{context_msg}\n"
+            "------------\n"
+            "新しいコンテキストを考慮して、元の答えをより良く洗練して質問に答えてください。\n"
+            "コンテキストが役に立たない場合は、元の回答と同じものを返します。")
 
-    #埋め込みモデルによるテキスト埋め込みを生成
-    #埋め込みベクトルを保持するためのリスト
-    docs = []
-    #文書のIDとテキストの対応を保持するための辞書
-    id_to_text_map = {}
-    #文書データを格納しているストレージコンテキストから文書の一覧を取得
-    for i, (_, node) in enumerate(storage_context.docstore.docs.items()):
-        #文書ノード（node）からテキストを取得
-        text = node.get_text()
-        #テキストの埋め込みを生成します
-        docs.append(embed_model.get_text_embedding(text))
-        id_to_text_map[i] = text
-    docs = np.array(docs)
+        refine_prompt = RefinePrompt(REFINE_PROMPT)
 
-    #text-ada-embedding-002から出力されるベクトル長を指定
-    d = 1536
-    index = faiss.IndexFlatL2(d)
-    #Faissにベクトルを登録
-    index.add(docs)
+        ################################################################
+        query_engine = index.as_query_engine(text_qa_template=qa_prompt, refine_template=refine_prompt)
 
-    # クエリとFaissから取り出すノード数の設定
-    query_text = question
-    k = 1
+        # query_engine = index.as_query_engine(text_qa_template=QA_PROMPT)
 
-    # questionのベクトル化
-    query = embed_model.get_text_embedding(query_text)
-    query=np.array([query])
+        # テンプレを送る
+        response = query_engine.query(question)
 
-    # Faissからのquestionに近いノードの取り出し2個
-    reader = FaissReader(index)
-    documents = reader.load_data(query=query, id_to_text_map=id_to_text_map, k=k)
+        #responseからtextとsourseの取り出し
+        response_text = response.response.replace("\n", "")
 
-    st.write(f'count_node: {len(documents)}')
+        def display_chat(chat_history):
+            for i, chat in enumerate(reversed(chat_history)):
+                if "user" in chat:
+                    message(chat["user"], is_user=True, key=str(i)) 
+                else:
+                    message(chat["bot"], key=f'bot{i}')
+                    
+        # 質問と応答をチャット履歴に追加
+        st.session_state["chat_history"].append({"user": question})
+        st.session_state["chat_history"].append({"bot": response_text})
 
-    
-    # デバッグ用
-    llama_debug_handler = LlamaDebugHandler()
-    callback_manager = CallbackManager([llama_debug_handler])
-    service_context = ServiceContext.from_defaults(callback_manager=callback_manager)
+        display_chat(st.session_state["chat_history"])
 
-    #Faissで確認した類似したノードを使って、GPTListIndexを作成。
-    # index = GPTListIndex.from_documents(documents, service_context=service_context)
-    index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
-
-    #質問用のQAプロンプトを生成
-    QA_PROMPT_TMPL = (
-        "私たちは以下の情報をコンテキスト情報として与えます。 \n"
-        "---------------------\n"
-        "{context_str}"
-        "\n---------------------\n"
-        "あなたはAIとして、この情報をもとに質問を日本語で答えます。前回と同じ回答の場合は同じ回答を行います。: {query_str}\n"
-    )
-    qa_prompt = QuestionAnswerPrompt(QA_PROMPT_TMPL)
-
-    ###############################################################tets
-    #回答要求用のプロンプトを生成
-    REFINE_PROMPT = ("元の質問は次のとおりです: {query_str} \n"
-        "既存の回答を提供しました: {existing_answer} \n"
-        "既存の答えを洗練する必要があります \n"
-        "(必要な場合のみ)以下にコンテキストを追加します。 \n"
-        "------------\n"
-        "{context_msg}\n"
-        "------------\n"
-        "新しいコンテキストを考慮して、元の答えをより良く洗練して質問に答えてください。\n"
-        "コンテキストが役に立たない場合は、元の回答と同じものを返します。")
-
-    refine_prompt = RefinePrompt(REFINE_PROMPT)
-
-    ################################################################
-    query_engine = index.as_query_engine(text_qa_template=qa_prompt, refine_template=refine_prompt)
-
-    # query_engine = index.as_query_engine(text_qa_template=QA_PROMPT)
-
-    # テンプレを送る
-    response = query_engine.query(question)
-
-    #responseからtextとsourseの取り出し
-    response_text = response.response.replace("\n", "")
-
-    def display_chat(chat_history):
-        for i, chat in enumerate(reversed(chat_history)):
-            if "user" in chat:
-                message(chat["user"], is_user=True, key=str(i)) 
-            else:
-                message(chat["bot"])
-                
-    # 質問と応答をチャット履歴に追加
-    st.session_state["chat_history"].append({"user": question})
-    st.session_state["chat_history"].append({"bot": response_text})
-
-    display_chat(st.session_state["chat_history"])
-
-    st.write('sources')
-    st.write(response.get_formatted_sources(length=1000))
+        st.write('sources')
+        st.write(response.get_formatted_sources(length=1000))
 
 def main():
     # アプリケーション名と対応する関数のマッピング
